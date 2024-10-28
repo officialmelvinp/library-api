4f00626f5a7038ce849b7d17338fed8873ea4ece
officialmelvinp <elmelvin@2002@gmail.com>
2024-10-28 05:25:38 +0100

    updated url configuration

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from library.routers import router  # Ensure the correct import
+from django.views.generic import RedirectView
 
 admin.site.site_header = "LIBRARY BY MELVINP"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),  # Ensure this path is correct
-    
+    path('', RedirectView.as_view(url='/api/v1/', permanent=False)),  # Redirect root to API
+    path('api/v1/', include(router.urls)),
 ]
